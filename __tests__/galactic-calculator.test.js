409a85982b23cc9a2e579b3e6a9aa1c3889f26e5
Graceann Smith <graceannsmith@gmail.com>
2023-12-23 11:14:28 -0800

    pass first jest test to recognise empty object

---
+++ __tests__/galactic-calculator.test.js
@@ -0,0 +1,9 @@
+import GalacticCalc from "./../src/galactic-calculator.js";
+
+describe('GalacticCalc', () => {
+test('should create an empty obect', () => {
+    const emptyObj = new GalacticCalc();
+    expect({}).toEqual({});
+})
+
+})
