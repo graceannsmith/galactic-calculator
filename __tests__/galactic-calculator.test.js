d7d5841c7018384cd0eef6e8b514cd4de9bccdc4
Graceann Smith <graceannsmith@gmail.com>
2023-12-25 01:03:46 -0800

    create jest test to recognise first property of GalacaticCalc()

--- __tests__/galactic-calculator.test.js
+++ __tests__/galactic-calculator.test.js
@@ -1,7 +1,15 @@
 import GalacticCalc from './../src/galactic-calculator.js';
+
 describe('GalacticCalc', () => {
 test('should create and empty object', () => {
 const emptyObject = new GalacticCalc();
 expect(emptyObject).toEqual({});
 });
+
+describe('GalacticCalc', () => {
+test('should create object with age property in Earth years', () => {
+    const testOne = new GalacticCalc(42);
+    expect(testOne).toEqual({currentEarthYear: 42})
+})
+})
 });
