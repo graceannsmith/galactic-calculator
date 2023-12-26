0027de6822ff45d4129ce050adbff53185d42d59
Graceann Smith <graceannsmith@gmail.com>
2023-12-25 23:50:40 -0800

    ake good fail for test of galactic years until target future birthday

--- src/galactic-calculator.js
+++ src/galactic-calculator.js
@@ -1,7 +1,7 @@
 export default class GalacticCalc {
     constructor(age, pastBirthday, futureBirthday) {
       this.age = this.calculatePlanetAges(age);
-      this.pastBirthday = pastBirthday;
+      this.pastBirthday = this.calculatePlanetAgesSinceTargetBirthday(age, pastBirthday);
       this.futureBirthday = futureBirthday;
     }
     calculatePlanetAges(age) {
@@ -12,5 +12,17 @@ export default class GalacticCalc {
           currentMarsYear: Math.floor(age / 1.88),
           currentJupiterYear: Math.floor(age / 11.86)
         };
+  }
+  calculatePlanetAgesSinceTargetBirthday(age, targetBirthday) {
+    const planetAges = this.calculatePlanetAges(age);
+    const targetAges = this.calculatePlanetAges(targetBirthday);
+    
+    return {
+      earthYearsSinceTargetBirthday: Math.floor(planetAges.currentEarthYear - targetAges.currentEarthYear),
+      mercuryYearsSinceTargetBirthday: Math.floor(planetAges.currentMercuryYear - targetAges.currentMercuryYear),
+      venusYearsSinceTargetBirthday: Math.floor(planetAges.currentVenusYear - targetAges.currentVenusYear),
+      marsYearsSinceTargetBirthday: Math.floor(planetAges.currentMarsYear - targetAges.currentMarsYear),
+      jupiterYearsSinceTargetBirthday: Math.floor(planetAges.currentJupiterYear - targetAges.currentJupiterYear)
+    };
   }
 }
