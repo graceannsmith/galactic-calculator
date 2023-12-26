41bb4b7e58c993f50b38413a476915a32c9a7f32
Graceann Smith <graceannsmith@gmail.com>
2023-12-25 21:45:46 -0800

    refactored code to pass test

--- src/galactic-calculator.js
+++ src/galactic-calculator.js
@@ -1,5 +1,8 @@
 export default class GalacticCalc {
-    
-    
+    constructor(age, pastBirthday, futureBirthday) {
+      this.age = age;
+      this.pastBirthday = pastBirthday;
+      this.futureBirthday = futureBirthday;
+    }
 
   }
