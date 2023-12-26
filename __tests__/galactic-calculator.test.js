79c5474b62edd1c4a37767d5724b43d116ff27d7
Graceann Smith <graceannsmith@gmail.com>
2023-12-25 21:57:30 -0800

    create new test for creating method to determine ages on different planets

--- __tests__/galactic-calculator.test.js
+++ __tests__/galactic-calculator.test.js
@@ -4,7 +4,16 @@ describe('Galactic Calculator' , () => {
     const test1 = new GalacticCalc (45, 35, 55)
     expect(test1.age).toEqual(45);
     expect(test1.pastBirthday).toEqual(35);
-    
 });
+test('should create an object with that displays age in earth, mercury,venus mars and jupiter years', () => {
+    const test2 = new GalacticCalc(42,35,55)
+    expect(test2.age).toEqual({
+        currentEarthYear: 42,
+        currentMercuryYear: 175,
+        currentVenusYear: 67,
+        currentMarsYear: 22,
+        currentJupiterYear: 3
+      })
+})
 
 });
